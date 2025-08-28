b97a30539cbefe70d533e731416c4d81b41a5e54
Saurabh Wani <saurabhwani78945@gmail.com>
2025-08-29 00:06:52 +0530

    Data injestion done

--- main.py
+++ main.py
@@ -1,3 +1,15 @@
 from chest_cancer_cnn_classifier import logger
+from chest_cancer_cnn_classifier.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
 
-logger.info("HI, welcome")
+STAGE_NAME = "Data Ingestion stage"
+
+
+try:
+    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+    obj = DataIngestionTrainingPipeline()
+    obj.main()
+    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+    logger.exception(e)
+    raise e
+
