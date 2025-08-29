f59f5387226f156707e42c6298ba8be2ecde159e
Saurabh Wani <saurabhwani78945@gmail.com>
2025-08-29 23:02:32 +0530

    prespare base model added

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from chest_cancer_cnn_classifier import logger
 from chest_cancer_cnn_classifier.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
+from chest_cancer_cnn_classifier.pipeline.stage_02_prepare_base_model import PrepareBaseModelTrainingPipeline
 
 STAGE_NAME = "Data Ingestion stage"
 
@@ -12,4 +13,17 @@ try:
 except Exception as e:
     logger.exception(e)
     raise e
+
+
+STAGE_NAME = "Prepare base model"
+
+try: 
+   logger.info(f"*******************")
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+   prepare_base_model = PrepareBaseModelTrainingPipeline()
+   prepare_base_model.main()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
 
